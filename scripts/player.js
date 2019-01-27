6cdd57cfab166b9c5ad2b026c4248b98e96c9bda
benmalartre <benmalartre@hotmail.com>
2019-01-27 19:34:51 +0100

    move element

--- scripts/player.js
+++ scripts/player.js
@@ -12,7 +12,7 @@ class Player extends Object{
         elem.style.display = 'block';
         elem.style.zIndex = 99;
         elem.style.top = Math.random() * 120;
-        elem.style.top = Math.random() * 120;
+        elem.style.left = Math.random() * 120;
         
         document.getElementById("GameContainer").appendChild(elem);
 
@@ -32,6 +32,11 @@ class Player extends Object{
         console.log("Jump : ");
         console.log(this.jump_counter);
         console.log("POSITION : "+this.x+", "+this.y);
+
+        this.x = Math.random() * 120;
+        this.y = Math.random() * 120;
+       this.elem.style.top = this.y;
+       this.elem.style.left = this.x;
     }
 
     
