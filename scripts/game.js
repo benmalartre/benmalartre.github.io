fce742aab6e9913900795d6678f680d7d3688ea2
benmalartre <benmalartre@hotmail.com>
2019-01-27 16:35:05 +0100

    wait loaded

--- scripts/game.js
+++ scripts/game.js
@@ -15,11 +15,12 @@ console.log(NUM_LOADING);
 function Game() {
     this.objects = new Array();
     this.player = null;
-    this.space_pressed = new KeyEvent('Space', player.j)
+    this.space_pressed = null;
 };
 
 Game.prototype.Init = function() {
     this.player = new Player(0,0);
+    this.space_pressed = new KeyEvent('Space', this.player.jump);
     for(var i=0;i<this.objects.length;i++){
         this.objects[i].Init();
     }
