7b16bfaebb60d7a103499e67cf8cc49c2d9e2c36
benmalartre <benmalartre@hotmail.com>
2021-05-15 21:58:56 +0200

    [github.io] test

--- content/softimage.js
+++ content/softimage.js
@@ -1,44 +1,26 @@
-//app.content.innerHTML = '<h1 style:"margin:auto";font-size:4em;color=white;text-align:center>ZOBI NIK VRAIMENT TOUT</h1>';
+alert('BOUGE!!!');
+var container = document.createElement('div');
+container.style.position = 'fixed';
+container.style.width = '800px';
+container.style.height = '600px';
+container.style.margin = 'auto';
+container.style.background = 'white';
+container.getElementsByTagName.zIndex = 12;
+app.content.Clear();
+app.content.Mount(container);
 
-var row = document.createElement('div');
-row.className = 'row';
+var elem = document.createElement('div');
+elem.style.position = 'relative';
+elem.style.width = '80px';
+elem.style.height = '60px';
+elem.style.background = 'black';
+container.appendChild(elem);
 
-var form = document.createElement('form');
-form.classList.add('payment-form');
-form.classList.add('col-lg-5');
-form.classList.add('collapse');
-form.id = 'payment-form';
-form.action = '/payment.html';
-form.method = 'post';
-
-var amount = document.createElement('input');
-amount.type = 'hidden';
-amount.name = 'amount';
-amount.value = '4200';
-
-var currency = document.createElement('input');
-currency.type = 'hidden';
-currency.name = 'currency';
-currency.value = 'EUR';
-
-var description = document.createElement('input');
-description.type = 'hidden';
-description.name = 'description';
-description.value = 'Easy Payments Guide!!!';
-
-var card = document.createElement('div')
-card.id = 'credit-card-fields'
-
-var btn = document.createElement('button');
-btn.classList.add('form-control');
-btn.classList.add('btn')
-btn.classList.add('btn-success') 
-btn.classList.add('submit-button')
-btn.type = 'submit'
-btn.textContent = 'Get your "Easy Payments Guide" now for 42â‚¬'
-
-form.appendChild(amount);
-form.appendChild(currency);
-form.appendChild(description);
-row.appendChild(form);
-app.content.elem.appendChild(row);
+for (var i = 1; i <= 64; i++) {
+    (function(index) {
+        setTimeout(function() { 
+            elem.style.left = index*10; 
+            elem.style.top = index*10;
+        }, i * 250);
+    })(i);
+}
