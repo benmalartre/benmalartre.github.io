621b42bbdace6124a55fd58e0625666f64ca5565
benmalartre <benmalartre@hotmail.com>
2023-12-02 17:47:50 +0100

    [app] cv wip

--- scripts/cv.js
+++ scripts/cv.js
@@ -2,46 +2,19 @@
 
 var buildCVCallback=function(cv, data) {
 	var descs = JSON.parse(data);
-	console.log(descs);
-	descs.forEach(function (desc, index) {
-		var callback = desc['key'];
-		var item = new CVItem_t(desc['key'], cv, index, callback);
-		cv.items.push(item);
-		cv.numEntries ++;
-	});
-}
-
-function CVItem_t(name, parent, id, callback){
-	this.elem = document.createElement('section');
-    this.place = document.createElement('div');
-    this.place.setAttribute('id', 'place');
-    this.place.setAttribute('innerHtml', 'zob');
+	let text = "<table border='1'>";
+    for (desc in descs) {
+    text += "<tr><td>" + desc.details + "</td></tr>";
+    }
+    text += "</table>"
+    cv.innerHTML = text;
     /*
-	this.text = document.createTextNode(name);
+	descs.forEach(function (desc, index) {
+  
+  
+	});
     */
-	this.elem.setAttribute('name', name);
-	this.elem.setAttribute('href', '#');
-    
-	this.elem.addEventListener('click', this.OnClick, false);
-    
-	this.elem.menu_id = id;
-	this.elem.callback = callback;
-	this.elem.parent = parent;
-
-    this.elem.appendChild(this.place);
-    parent.elem.appendChild(this.elem);
-    alert(name);
-	
 }
-
-CVItem_t.prototype.OnClick = function(event){
-	var cvItem = event.target;
-	var cvParent = cvItem.parent;
-	var id = event.target.menu_id;
-
-    alert('menu clicked!!!');
-    alert(id);
-};
 
 function CV_t(parent) {
 	this.numEntries = 0;
