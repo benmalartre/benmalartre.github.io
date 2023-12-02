3b3bae1f68b9430f2d7dc1459ade3cd196d40d50
benmalartre <benmalartre@hotmail.com>
2023-12-02 19:43:51 +0100

    [css] update index css

--- content/home.js
+++ content/home.js
@@ -12,6 +12,15 @@ function buildHome() {
     canvas.style.width = '100%';
     canvas.style.height = '100%';
 
+    canvas.style.imageRendering = "optimizeSpeed";             /* Legal fallback */
+	canvas.style.imageRendering = "-moz-crisp-edges";          /* Firefox        */
+	canvas.style.imageRendering = "-o-crisp-edges";            /* Opera          */
+	canvas.style.imageRendering = "-webkit-optimize-contrast"; /* Safari         */
+	canvas.style.imageRendering = "optimize-contrast";         /* CSS3 Proposed  */
+	canvas.style.imageRendering = "crisp-edges";               /* CSS4 Proposed  */
+	canvas.style.imageRendering = "pixelated";                 /* CSS4 Proposed  */
+	canvas.style.msInterpolationMode = "nearest-neighbor";     /* IE8+           */
+
     content.appendChild(canvas);
 
     app.content.Clear();
