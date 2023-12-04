3a00972ea367a3a2c8c73f08b11ba52f3f336329
benmalartre <benmalartre@hotmail.com>
2023-12-04 20:14:01 +0100

    [content] remove unused code

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,7 @@
 "use strict";
-var LASTX = 0;
-var LASTY = 0;
-var OFFSETX = 0;
-var OFFSETY = 0;
-var DRAG = false;
-var NEWX = 0;
-var NEWY = 0;
 
-var INFO_SELECT_DATE = 'Choisissez une date :'
-
-function Content_t(elem){
-    this.parentElement = elem;
+function Content_t(parent){
+    this.parentElement = parent;
     this.elem = document.createElement('div');
     this.elem.id = 'content_slider';
     this.elem.style.position = 'relative';
@@ -19,59 +10,16 @@ function Content_t(elem){
     this.elem.style.width = '100%';
     this.elem.style.height = '100%';
     this.elem.style.touchAction = 'none';
-    elem.appendChild(this.elem);
+    parent.appendChild(this.elem);
 
-    this.drag = false;
-    this.data = null;
+    this.objects = new Array();
 };
 
 Content_t.prototype.Update = function() {
-    if(this.data)this.data.Update();
+    for(var i=0; i < this.objects.length; i++) {
+        this.objects[i].Update();
+    }
 }
-
-Content_t.prototype.OnMouseDown = function(ev){
-    DRAG = true;
-    if(ev.type === 'touchstart'){
-        LASTX = ev.touches[0].clientX - OFFSETX;
-        LASTY = ev.touches[0].clientY - OFFSETY;
-    }
-    else{
-        LASTX = ev.clientX - OFFSETX;
-        LASTY = ev.clientY - OFFSETY;
-    }
-};
-
-Content_t.prototype.OnMouseUp = function(ev){
-    DRAG = false;
-    OFFSETX = NEWX;
-    OFFSETY = NEWY;
-};
-
-Content_t.prototype.OnMouseMove = function(ev){
-    ev.preventDefault();
-    if(DRAG){
-        var win = document.querySelector('#content');
-        var elem = document.querySelector('#content_slider');
-        if(ev.type === 'touchmove'){
-            NEWX = ev.touches[0].clientX - LASTX;
-            NEWY = ev.touches[0].clientY - LASTY;
-        }
-        else{
-            NEWX = ev.screenX - LASTX;
-            NEWY = ev.screenY - LASTY;
-        }
-        
-        var maxX = -(elem.offsetWidth-win.offsetWidth);
-        var maxY = -(elem.offsetHeight-win.offsetHeight)
-        if(NEWX > 0)NEWX = 0;
-        else if(NEWX<maxX)NEWX = maxX;
-        if(NEWY > 0)NEWY = 0;
-        else if(NEWY<maxY)NEWY = maxY;
-
-        elem.style.left = NEWX;
-        elem.style.top = NEWY;
-    }
-};
 
 Content_t.prototype.Clear = function(){
     for(var i=this.elem.childNodes.length-1;i>=0;i--){
@@ -79,19 +27,11 @@ Content_t.prototype.Clear = function(){
     }
 };
 
-Content_t.prototype.Mount = function(elem){
-    this.elem.appendChild(elem);
+Content_t.prototype.Mount = function(object){
+    this.elem.appendChild(object.elem);
     this.elem.style.width = elem.offsetWidth;
     this.elem.style.wheightidth = elem.offsetHeight;
     this.elem.top = '0px';
     this.elem.left = '0px';
-};
-
-Content_t.prototype.SetData = function(data){
-    this.data = data;
-};
-
-Content_t.prototype.Scroll = function(newX, newY){
-    this.parentElement.scrollLeft = newX;
-    this.parentElement.scrollTop = newY;
+    this.objects.push(object);
 };
--- scripts/misc.js
+++ scripts/misc.js
@@ -151,3 +151,14 @@ function init() {
         var actual_JSON = JSON.parse(response);
     });
 }
+
+
+function addImage(filename, parentname){
+	this.img = document.createElement("img");
+	this.img.src = filename;
+	if(parentname){
+		src = getElementById(parentname);
+		src.appendChild(this.img)
+	}
+	else document.body.appendChild(this.img);
+}
