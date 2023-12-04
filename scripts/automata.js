d56d8468211a002ac6933a92f313d96f4d9b13db
benmalartre <benmalartre@hotmail.com>
2023-12-04 12:06:19 +0100

    [content] remove unused code

--- scripts/automata.js
+++ scripts/automata.js
@@ -145,7 +145,6 @@ function CellularAutomata(nbx,nby)
 	this.LastLine = function()
 	{
 		if(this.counter%200==0){
-			//this.rule =(this.rule+1)%3;
 			this.color.r = Math.random()*155;
 			this.color.g = Math.random()*155;
 			this.color.b = Math.random()*155;
@@ -157,10 +156,8 @@ function CellularAutomata(nbx,nby)
 		
 		for(var x=0;x<this.nbx;x++)
 		{
-			left = x > 0 ? x-1 : this.nbx-1;
-			right = x <= this.nbx ? x+1 : 0;
-			if(left<0)left = this.nbx-1;
-			if(right>(this.nbx-1))right = 0;
+			left = x > 0 ? x-1 : x;
+			right = x <= this.nbx ? x+1 : x;
 			
 			var a = row.cells[left].alive;
 			var b = row.cells[x].alive;
