b92efac75e862a91d75626736adbb7909b15bdf2
benmalartre <benmalartre@hotmail.com>
2023-12-05 10:49:59 +0100

    [content] remove unused code

--- content/softimage.js
+++ content/softimage.js
@@ -1,23 +1,11 @@
-var container = document.createElement('div');
-container.style.position = 'fixed';
-container.style.width = '800px';
-container.style.height = '600px';
-container.style.margin = 'auto';
-container.style.background = 'white';
-container.getElementsByTagName.zIndex = 12;
-app.content.Clear();
-app.content.Mount(container);
+function buildProjects() {
+    let projects = new Projects_t();
 
-var elem = document.createElement('div');
-elem.style.position = 'relative';
-elem.style.width = '80px';
-elem.style.height = '60px';
-elem.style.background = 'orange';
-container.appendChild(elem);
-
-for (var i = 1; i <= 64; i++) {
-    setTimeout(function(elem, t) { 
-        elem.style.left = t*10+'px'; 
-        elem.style.top = t*10+'px';
-    }.bind(this, elem, i), i*250);
+    app.content.Clear();
+    app.content.Mount(projects);
 }
+
+includeCSS('css/projects.css');
+includeScript('scripts/projects.js', buildProjects);
+
+
---
+++ scripts/projects.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var buildProjectsCallback=function(projects, data) {
+    var descs = JSON.parse(data);
+    //let text = "<table border='1'>";
+    
+    descs.forEach(function (desc, index) {
+
+        /*
+
+        "name": "toonix",      
+        "label": "Toonix",      
+        "description": "Toonix is a set of custom C++ ICE Nodes to generate Toon Lines, it supports Silhouette Detection, Boundary Detection,Intersection Detection(WIP) Crease Detection, Edge Cluster Detection, as well as experimental light and shadow approximation. It outputs on-the-fly polymesh geometry.",
+        "thumb": "../images/softimage/toonix.jpg",
+        "url": "https://github.com/benmalartre/Toonix"
+
+        */
+        //text += "<tr><td>" + desc.details + "</td></tr>";
+        var project = document.createElement('section');
+        var name = document.createElement('h1');
+        name.innerHTML = desc.label;
+
+        var description = document.createElement('div');
+        description.innerHTML = desc.description;
+
+        var thumb = document.createElement('img');
+        thumb.src = desc.thumb;
+
+        var link = document.createElement('a');
+        var url = document.createTextNode(desc.name);
+        link.appendChild(url);
+        link.title = desc.url;
+        link.href = desc.url;
+
+        section.appendChild(name);
+        section.appendChild(description);
+        section.appendChild(thumb);
+
+        projects.elem.appendChild(project);
+
+    });
+
+    //text += "</table>";
+    //cv.elem.innerHTML = text;
+}
+
+function Projects_t() {
+	this.numEntries = 0;
+	this.items = new Array();
+	this.elem = document.createElement('div');
+    this.elem.id = 'projects';
+	loadJSON(this, buildProjectsCallback, 'datas/softimage.json', true);
+
+    this.Update = function(){};
+    return this;
+};
