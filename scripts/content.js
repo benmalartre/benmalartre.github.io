1c86256dbe32b7637065fbf0155d9c9441901197
benmalartre <benmalartre@hotmail.com>
2023-12-05 09:36:50 +0100

    [content] remove unused code

--- scripts/content.js
+++ scripts/content.js
@@ -27,11 +27,11 @@ Content_t.prototype.Clear = function(){
     }
 };
 
-Content_t.prototype.Mount = function(object){   
-    this.elem.appendChild(object.elem);
+Content_t.prototype.Mount = function(content){   
+    this.elem.appendChild(content.elem);
     this.elem.style.width = elem.offsetWidth;
     this.elem.style.wheightidth = elem.offsetHeight;
     this.elem.top = '0px';
     this.elem.left = '0px';
-    this.objects.push(object);
+    this.objects.push(content);
 };
