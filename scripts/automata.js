16c72d2cf6efff8a2295d5f08b7eb79008b9f067
benmalartre <benmalartre@hotmail.com>
2025-01-12 18:33:24 +0100

    automata remove unwanted log

--- scripts/automata.js
+++ scripts/automata.js
@@ -144,7 +144,6 @@ function CellularAutomata(nbx,nby)
 
 	this.Line = function(idx, rule)
 	{
-		console.log("line : " + idx);
 		if(!idx) {
 			var row = this.grid.rows[0];
 			for(var x=0;x<this.nbx+2;x++)
